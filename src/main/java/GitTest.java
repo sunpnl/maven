238f6d7c92c710e2c52f847a01ad7d85c91b649d
sunPnl <1024748651@qq.com>
2023-07-30 17:35:27 +0800

    master commit3

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
         System.out.println("Hello Git!2");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("hot-fix");
     }
 }
